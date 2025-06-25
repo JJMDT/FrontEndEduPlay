fc95ed40681807a821ceeb2a2709d0650f6681d7
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-06-24 23:09:07 -0300

    Update carta-pregunta.ts

--- src/app/components/carta-pregunta/carta-pregunta.ts
+++ src/app/components/carta-pregunta/carta-pregunta.ts
@@ -39,7 +39,7 @@ export class CartaPreguntaComponent implements OnInit {
   async obtenerPregunta(): Promise<boolean> {
     let res: boolean = false;
     try {
-      this.pregunta = await firstValueFrom(this.http.get<Pregunta>('http://localhost:3000/pregunta/traer'));
+      this.pregunta = await firstValueFrom(this.http.get<Pregunta>(http://44.202.218.94:3000/pregunta/traer'));
       //console.log('Pregunta:', this.pregunta);
       res = true;
     } catch (error) {
@@ -99,7 +99,7 @@ export class CartaPreguntaComponent implements OnInit {
     try {
       this.resultado = await firstValueFrom(
         this.http.post<PuntosResultado>(
-          'http://localhost:3000/pregunta/puntuacion',
+          http://44.202.218.94:3000/pregunta/puntuacion',
           body
         )
       );
